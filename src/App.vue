be8e7ccc9e52244d1ca3a4f6910b37d1a5d9b764
Anton Pushkarev <gridmaniac@gmail.com>
2021-03-06 18:46:50 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-app-bar app color="gray" dark>
+      <div class="text-h4">Zones</div>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+
+  components: {
+    HelloWorld
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
